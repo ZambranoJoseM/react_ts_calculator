b08d706038eb115a63b95ed7406dbf893c495604
Jose Zambrano <josezambrano@EN1710062.local>
2022-01-28 13:17:42 -0300

    structure and addition of number buttons

---
+++ calculator/src/components/Calculator.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+
+const Container = styled.div`
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(4, 80px);
+  grid-template-rows: repeat(5, 80px);
+`;
+
+const Calculator: React.FC<{}> = () => {
+    return (
+        <Container>
+            <Grid>
+                <Button label="9"/>
+                <Button label="8"/>
+                <Button label="7"/>
+                <Button label="6"/>
+                <Button label="5"/>
+                <Button label="4"/>
+                <Button label="3"/>
+                <Button label="2"/>
+                <Button label="1"/>
+            </Grid>
+        </Container>
+    )
+}
+
+export default Calculator
