a0cbae71f33aebf75893701e0d3db78bc4bcd6b4
Jose Zambrano <josezambrano@EN1710062.local>
2022-02-01 10:38:38 -0300

    Finishing all UI with styled components and grid

--- calculator/src/components/Button.tsx
+++ calculator/src/components/Button.tsx
@@ -2,18 +2,46 @@ import React from "react";
 import styled from "styled-components";
 
 
+export enum ButtonType {
+
+    Number,
+    Operation,
+
+}
+
 type Props = {
+    type?: ButtonType
     label: string
+    position?: [x: number, y: number]
+    width?: number
+    height?: number
 }
 const StyledButton = styled.button`
-  background: #e48900;
+  background: #727171;
   border: none;
   border-radius:8px;
+  font-size: 24px;
+  color: #fff;
 `;
 
-const Button: React.FC<Props> = ({label}) => {
+const Button: React.FC<Props> = ({label, position, width, height, type=ButtonType.Operation}) => {
+const styles: React.CSSProperties = {}
+    if(position){
+        styles.gridColumnStart= position[0] + 1
+        styles.gridRowStart= position[1] + 1
+    }
+    if(width){
+        styles.gridColumnEnd = `span ${width}`
+    }
+    if(height){
+        styles.gridRowEnd = `span ${height}`
+    }
+    if(type === ButtonType.Number){
+        styles.color = "#000"
+        styles.background = "#e48900"
+    }
 return(
-   <StyledButton>{label}</StyledButton>
+   <StyledButton style = {styles}>{label}</StyledButton>
 )
 
 }
